19afe2295f532893b902d5032f491dab74509bc9
Jeremy Thil <thil.jeremy@gmail.com>
2020-05-06 13:44:17 +0200

    use usecase mapper

--- app/src/main/java/com/stardeux/upprime/latest/usecase/GetLatestUseCase.kt
+++ app/src/main/java/com/stardeux/upprime/latest/usecase/GetLatestUseCase.kt
@@ -1,11 +1,12 @@
 package com.stardeux.upprime.latest.usecase
 
 import com.stardeux.upprime.latest.repository.LatestRepository
-import com.stardeux.upprime.latest.repository.model.MediaPageResponse
+import com.stardeux.upprime.latest.usecase.mapper.mapToMediaPage
+import com.stardeux.upprime.latest.usecase.model.MediaPage
 
 class GetLatestUseCase(private val latestRepository: LatestRepository) {
 
-    suspend fun getLatest(): MediaPageResponse {
-        return latestRepository.getNew()
+    suspend fun getLatest(): MediaPage {
+        return mapToMediaPage(latestRepository.getNew())
     }
 }
--- app/src/main/java/com/stardeux/upprime/latest/usecase/mapper/MediaItemResponseMapper.kt
+++ app/src/main/java/com/stardeux/upprime/latest/usecase/mapper/MediaItemResponseMapper.kt
@@ -3,7 +3,9 @@ package com.stardeux.upprime.latest.usecase.mapper
 import com.stardeux.upprime.core.mapper.mapToDate
 import com.stardeux.upprime.core.mapper.mapToMediaType
 import com.stardeux.upprime.latest.repository.model.MediaItemResponse
+import com.stardeux.upprime.latest.repository.model.MediaPageResponse
 import com.stardeux.upprime.latest.usecase.model.MediaItem
+import com.stardeux.upprime.latest.usecase.model.MediaPage
 
 fun mapToMediaItem(mediaItemResponse: MediaItemResponse): MediaItem {
     return MediaItem(
@@ -12,5 +14,13 @@ fun mapToMediaItem(mediaItemResponse: MediaItemResponse): MediaItem {
         imdbId = requireNotNull(mediaItemResponse.imdbId),
         dateAdded = mapToDate(requireNotNull(mediaItemResponse.dateAdded)),
         type = mapToMediaType(requireNotNull(mediaItemResponse.type))
+    )
+}
+
+fun mapToMediaPage(mediaPageResponse: MediaPageResponse): MediaPage {
+    return MediaPage(
+        count = requireNotNull(mediaPageResponse.count),
+        medias = requireNotNull(mediaPageResponse.results).map(::mapToMediaItem)
+
     )
 }
