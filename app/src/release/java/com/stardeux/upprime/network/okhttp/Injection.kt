f35912f3b62dd0e2b7651dff4ba7dac6b4f4ee1c
Jeremy Thil <thil.jeremy@gmail.com>
2020-05-07 22:49:48 +0200

    Release compat for tmdb api

--- app/src/release/java/com/stardeux/upprime/network/okhttp/Injection.kt
+++ app/src/release/java/com/stardeux/upprime/network/okhttp/Injection.kt
@@ -2,6 +2,8 @@ package com.stardeux.upprime.network.okhttp
 
 import com.stardeux.upprime.network.amazon.di.AMAZON_NAMED_QUALIFIER
 import com.stardeux.upprime.network.amazon.di.amazonNetworkModule
+import com.stardeux.upprime.network.tmdb.di.TMDB_NAMED_QUALIFIER
+import com.stardeux.upprime.network.tmdb.di.tmdbNetworkModule
 import okhttp3.OkHttpClient
 import org.koin.core.qualifier.named
 import org.koin.dsl.module
@@ -10,6 +12,10 @@ val commonNetwork = module { }
 
 val fullAmazonNetwork = amazonNetworkModule.apply {
     single(named(AMAZON_NAMED_QUALIFIER)) { provideOkHttp(get(named(AMAZON_NAMED_QUALIFIER))) }
+}
+
+val fullTmdbNetwork = tmdbNetworkModule.apply {
+    single(named(TMDB_NAMED_QUALIFIER)) { provideOkHttp(get(named(TMDB_NAMED_QUALIFIER))) }
 }
 
 fun provideOkHttp(
