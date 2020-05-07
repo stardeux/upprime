fb18fc64035aeee8e345d6bfa5592228ecf16d89
Jeremy Thil <thil.jeremy@gmail.com>
2020-05-07 22:44:32 +0200

    TMDB authenticator

--- app/src/main/java/com/stardeux/upprime/network/tmdb/TmdbAuthenticatorInterceptor.kt
+++ app/src/main/java/com/stardeux/upprime/network/tmdb/TmdbAuthenticatorInterceptor.kt
@@ -6,8 +6,11 @@ import okhttp3.Response
 class TmdbAuthenticatorInterceptor : Interceptor {
 
     override fun intercept(chain: Interceptor.Chain): Response {
+        val url = chain.request().url.newBuilder().addQueryParameter("api_key", TmdbApiConst.API_KEY).build()
+
         val finalRequest = chain.request().newBuilder()
             .addHeader("api_key", TmdbApiConst.API_KEY)
+            .url(url)
             .build()
 
         return chain.proceed(finalRequest)
