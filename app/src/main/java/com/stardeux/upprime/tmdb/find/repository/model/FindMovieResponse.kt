d51659f2cc60fff76554b284eeacbc4602a0242d
Jeremy Thil <thil.jeremy@gmail.com>
2020-05-09 12:29:58 +0200

    Find series response

--- app/src/main/java/com/stardeux/upprime/tmdb/find/repository/model/FindMovieResponse.kt
+++ app/src/main/java/com/stardeux/upprime/tmdb/find/repository/model/FindMovieResponse.kt
@@ -4,9 +4,10 @@ import com.google.gson.annotations.SerializedName
 
 data class FindMovieResponse(
     @SerializedName("id") val tmdbId: String?,
-    @SerializedName("vote_average") val voteAverage: Float?,
     @SerializedName("title") val title: String?,
-    @SerializedName("release_date") val releaseDate: String?,
+    @SerializedName("original_title") val originalTitle: String?,
+    @SerializedName("poster_path") val posterPath: String?,
     @SerializedName("original_language") val originalLanguage: String?,
-    @SerializedName("original_title") val originalTitle: String
+    @SerializedName("release_date") val releaseDate: String?,
+    @SerializedName("vote_average") val voteAverage: Float?
 )
---
+++ app/src/main/java/com/stardeux/upprime/tmdb/find/repository/model/FindSeriesResponse.kt
@@ -0,0 +1,14 @@
+package com.stardeux.upprime.tmdb.find.repository.model
+
+import com.google.gson.annotations.SerializedName
+
+data class FindSeriesResponse(
+    @SerializedName("id") val tmdbId: String?,
+    @SerializedName("original_name") val originalName: String?,
+    @SerializedName("name") val name: String?,
+    @SerializedName("poster_path") val posterPath: String?,
+    @SerializedName("original_language") val originalLanguage: String,
+    @SerializedName("origin_country") val originCountry: String,
+    @SerializedName("first_air_date") val firstAirDate: String?,
+    @SerializedName("vote_average") val voteAverage: Float?
+)
