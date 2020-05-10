0891a69b7090723fffa9453816d6d35183ae84a5
Jeremy Thil <thil.jeremy@gmail.com>
2020-05-10 23:13:06 +0200

    country

---
+++ app/src/main/java/com/stardeux/upprime/country/ui/SelectCountryActivity.kt
@@ -0,0 +1,7 @@
+package com.stardeux.upprime.country.ui
+
+import androidx.appcompat.app.AppCompatActivity
+
+class SelectCountryActivity : AppCompatActivity() {
+
+}
---
+++ app/src/main/java/com/stardeux/upprime/country/ui/SelectCountryFragment.kt
@@ -0,0 +1,5 @@
+package com.stardeux.upprime.country.ui
+
+import androidx.fragment.app.Fragment
+
+class SelectCountryFragment : Fragment()
