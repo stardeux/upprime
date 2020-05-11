41a7f55cef620b71f370c721325fb91d7883e38c
Jeremy Thil <thil.jeremy@gmail.com>
2020-05-11 23:09:20 +0200

    provide

--- app/src/main/java/com/stardeux/upprime/country/di/Injection.kt
+++ app/src/main/java/com/stardeux/upprime/country/di/Injection.kt
@@ -1,9 +1,11 @@
 package com.stardeux.upprime.country.di
 
+import android.content.SharedPreferences
+import com.stardeux.upprime.country.repository.UserLocaleRepository
 import com.stardeux.upprime.country.ui.SelectCountryViewModel
 import com.stardeux.upprime.country.usecase.GetAvailableCountryUseCase
 import com.stardeux.upprime.country.usecase.GetFlagUrlUseCase
-import com.stardeux.upprime.country.usecase.GetMyCountryUseCase
+import com.stardeux.upprime.country.usecase.SelectedUserLocaleUseCase
 import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
 
@@ -11,7 +13,17 @@ val countryModule = module {
     viewModel { provideSelectCountryViewModel(get(), get()) }
     factory { provideGetAvailableCountryUseCase() }
     factory { provideGetFlagUrlUseCase() }
-    factory { provideGetMyCountryUseCase() }
+
+    factory { provideUserLocaleRepository(get()) }
+    factory { provideSelectedUserLocaleUseCase(get()) }
+}
+
+private fun provideSelectedUserLocaleUseCase(userLocaleRepository: UserLocaleRepository): SelectedUserLocaleUseCase {
+    return SelectedUserLocaleUseCase(userLocaleRepository)
+}
+
+private fun provideUserLocaleRepository(sharedPreferences: SharedPreferences): UserLocaleRepository {
+    return UserLocaleRepository(sharedPreferences)
 }
 
 private fun provideSelectCountryViewModel(
@@ -26,8 +38,4 @@ private fun provideGetAvailableCountryUseCase(): GetAvailableCountryUseCase {
 
 private fun provideGetFlagUrlUseCase(): GetFlagUrlUseCase {
     return GetFlagUrlUseCase()
-}
-
-private fun provideGetMyCountryUseCase(): GetMyCountryUseCase {
-    return GetMyCountryUseCase()
 }
