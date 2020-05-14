3415bc1dd54d17d7c389afe6933c1e3bbb6a2376
Jeremy Thil <thil.jeremy@gmail.com>
2020-05-14 15:55:50 +0200

    start media fiche

---
+++ app/src/main/java/com/stardeux/upprime/media/fiche/MediaFicheActivity.kt
@@ -0,0 +1,8 @@
+package com.stardeux.upprime.media.fiche
+
+import androidx.appcompat.app.AppCompatActivity
+import com.stardeux.upprime.R
+
+class MediaFicheActivity : AppCompatActivity(R.layout.activity_media_fiche) {
+
+}
