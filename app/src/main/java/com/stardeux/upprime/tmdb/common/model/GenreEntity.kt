4adef71dc2fbe41355a4e785abd3d4267b53c919
Jeremy Thil <thil.jeremy@gmail.com>
2020-05-15 22:07:14 +0200

    db model

---
+++ app/src/main/java/com/stardeux/upprime/tmdb/common/model/GenreEntity.kt
@@ -0,0 +1,22 @@
+package com.stardeux.upprime.tmdb.common.model
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import com.stardeux.upprime.tmdb.common.model.GenreEntity.Companion.TABLE_GENRE_NAME
+
+@Entity(tableName = TABLE_GENRE_NAME)
+class GenreEntity(
+    @PrimaryKey(autoGenerate = true) @ColumnInfo(name = COL_GENRE_ID) val id: Int = 0,
+    @ColumnInfo(name = COL_GENRE_TMDB_ID) val tmdbId: String?,
+    @ColumnInfo(name = COL_GENRE_NAME) val name: String?
+) {
+
+    companion object {
+        const val TABLE_GENRE_NAME = "genres"
+        const val COL_GENRE_ID = "genre_id"
+        const val COL_GENRE_TMDB_ID = "genre_tmdb_id"
+        const val COL_GENRE_NAME = "genre_name"
+    }
+
+}
--- app/src/main/java/com/stardeux/upprime/tmdb/movie/repository/model/TmdbMovieDetailsResponse.kt
+++ app/src/main/java/com/stardeux/upprime/tmdb/movie/repository/model/TmdbMovieDetailsResponse.kt
@@ -2,7 +2,7 @@ package com.stardeux.upprime.tmdb.movie.repository.model
 
 import com.google.gson.annotations.SerializedName
 import com.stardeux.upprime.tmdb.common.model.GenreResponse
-import com.stardeux.upprime.tmdb.common.model.ProductionCountriesResponse
+import com.stardeux.upprime.tmdb.common.model.ProductionCountryResponse
 
 data class TmdbMovieDetailsResponse(
     @SerializedName("id") val tmdbId: String?,
@@ -13,7 +13,7 @@ data class TmdbMovieDetailsResponse(
     @SerializedName("release_date") val releaseDate: String?,
     @SerializedName("runtime") val runtime: Int?,
     @SerializedName("genres") val genres: List<GenreResponse>?,
-    @SerializedName("production_countries") val productionCountries: List<ProductionCountriesResponse>?,
+    @SerializedName("production_countries") val productionCountries: List<ProductionCountryResponse>?,
     @SerializedName("vote_average") val voteAverage: Float?,
     @SerializedName("vote_count") val voteCount: Int?,
     @SerializedName("overview") val synopsis: String?,
