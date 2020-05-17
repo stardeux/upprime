8ec6cd86b3992c79f5e63f83ff1407baf973b3cc
Jeremy Thil <thil.jeremy@gmail.com>
2020-05-17 19:11:22 +0200

    split seris repo

--- app/src/main/java/com/stardeux/upprime/series/repository/SeriesRepository.kt
+++ app/src/main/java/com/stardeux/upprime/series/repository/SeriesRepository.kt
@@ -1,6 +1,8 @@
 package com.stardeux.upprime.series.repository
 
+import com.stardeux.upprime.core.model.ImdbId
 import com.stardeux.upprime.series.repository.api.SeriesRemoteDataSource
+import com.stardeux.upprime.series.repository.database.SeriesDao
 import com.stardeux.upprime.series.repository.database.SeriesLocalDataSource
 import com.stardeux.upprime.series.repository.mapper.SeriesDetailsMapper
 import com.stardeux.upprime.series.repository.mapper.mapToSeriesDetailsEntity
@@ -13,15 +15,30 @@ class SeriesRepository(
     private val seriesDetailsMapper: SeriesDetailsMapper
 ) {
 
-    suspend fun getSeriesDetails(tmdbSeriesRequest: TmdbSeriesRequest, language: String): SeriesDetails {
-        val cached = seriesLocalDataSource.getSeries(tmdbSeriesRequest.imdbId)
-        return if (cached != null) {
-            seriesDetailsMapper.mapToSeriesDetails(cached)
-        } else {
-            seriesDetailsMapper.mapToSeriesDetails(
-                seriesRemoteDataSource.getSeriesDetails(tmdbSeriesRequest.tmdbId, language),
-                tmdbSeriesRequest
-            ).also { seriesLocalDataSource.insert(mapToSeriesDetailsEntity(it)) }
+    suspend fun getSeriesDetails(
+        tmdbSeriesRequest: TmdbSeriesRequest, language: String
+    ): SeriesDetails {
+        val cached = getCachedSeriesDetails(tmdbSeriesRequest.imdbId)
+        return cached ?: getRemoteSeriesDetails(tmdbSeriesRequest, language).also {
+            cacheSeriesDetails(it)
         }
+    }
+
+    suspend fun cacheSeriesDetails(seriesDetails: SeriesDetails) {
+        seriesLocalDataSource.insert(mapToSeriesDetailsEntity(seriesDetails))
+    }
+
+    suspend fun getRemoteSeriesDetails(
+        tmdbSeriesRequest: TmdbSeriesRequest, language: String
+    ): SeriesDetails {
+        return seriesDetailsMapper.mapToSeriesDetails(
+            seriesRemoteDataSource.getSeriesDetails(tmdbSeriesRequest.tmdbId, language),
+            tmdbSeriesRequest
+        )
+    }
+
+    suspend fun getCachedSeriesDetails(imdbId: ImdbId): SeriesDetails? {
+        return seriesLocalDataSource.getSeries(imdbId)
+            ?.let { seriesDetailsMapper.mapToSeriesDetails(it) }
     }
 }
