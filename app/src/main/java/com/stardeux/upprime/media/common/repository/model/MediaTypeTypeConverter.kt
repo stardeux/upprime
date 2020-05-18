2b32ac4aaaf3df811e8ccf79896d42c5fa73278d
Jeremy Thil <thil.jeremy@gmail.com>
2020-05-18 14:10:33 +0200

    create latest entity

---
+++ app/src/main/java/com/stardeux/upprime/media/common/repository/model/MediaTypeTypeConverter.kt
@@ -0,0 +1,30 @@
+package com.stardeux.upprime.media.common.repository.model
+
+import androidx.room.TypeConverter
+import com.stardeux.upprime.core.model.MediaType
+
+object MediaTypeTypeConverter {
+
+    private const val MEDIA_TYPE_MOVIE_DB_VALUE = "movie"
+    private const val MEDIA_TYPE_SERIES_DB_VALUE = "series"
+
+    @TypeConverter
+    @JvmStatic
+    fun mediaTypeToDatabaseValue(mediaType: MediaType): String {
+        return when (mediaType) {
+            MediaType.MOVIE -> MEDIA_TYPE_MOVIE_DB_VALUE
+            MediaType.SERIES -> MEDIA_TYPE_SERIES_DB_VALUE
+        }
+    }
+
+    @TypeConverter
+    @JvmStatic
+    fun databaseValueToMediaType(databaseValue: String): MediaType {
+        return when (databaseValue) {
+            MEDIA_TYPE_MOVIE_DB_VALUE -> MediaType.MOVIE
+            MEDIA_TYPE_SERIES_DB_VALUE -> MediaType.SERIES
+            else -> throw IllegalArgumentException("Unknown database value $databaseValue")
+        }
+    }
+
+}
---
+++ app/src/main/java/com/stardeux/upprime/media/latest/repository/database/LatestMediaEntity.kt
@@ -0,0 +1,28 @@
+package com.stardeux.upprime.media.latest.repository.database
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import com.stardeux.upprime.core.model.AmazonId
+import com.stardeux.upprime.core.model.ImdbId
+import com.stardeux.upprime.core.model.MediaType
+import com.stardeux.upprime.media.latest.repository.database.LatestMediaEntity.Companion.TABLE_LATEST_NAME
+
+@Entity(tableName = TABLE_LATEST_NAME)
+data class LatestMediaEntity(
+    @PrimaryKey @ColumnInfo(name = COL_AMAZON_ID) val amazonId: AmazonId,
+    @ColumnInfo(name = COL_TITLE) val title: String?,
+    @ColumnInfo(name = COL_IMDB_ID) val imdbId: ImdbId,
+    @ColumnInfo(name = COL_DATE_ADDED) val dateAdded: String,
+    @ColumnInfo(name = COL_MEDIA_TYPE) val type: MediaType
+) {
+
+    companion object {
+        const val TABLE_LATEST_NAME = "latest"
+        const val COL_AMAZON_ID = "amazon_id"
+        const val COL_TITLE = "title"
+        const val COL_IMDB_ID = "imdb_id"
+        const val COL_DATE_ADDED = "date_added"
+        const val COL_MEDIA_TYPE = "media_type"
+    }
+}
