575cdbfc6e167cd17e1b5f463431153b56a1d333
Jeremy Thil <thil.jeremy@gmail.com>
2020-05-20 15:35:34 +0200

    inject video

--- app/src/main/java/com/stardeux/upprime/tmdb/video/di/Injection.kt
+++ app/src/main/java/com/stardeux/upprime/tmdb/video/di/Injection.kt
@@ -3,19 +3,32 @@ package com.stardeux.upprime.tmdb.video.di
 import com.stardeux.upprime.network.tmdb.di.TMDB_NAMED_QUALIFIER
 import com.stardeux.upprime.tmdb.video.repository.VideoRepository
 import com.stardeux.upprime.tmdb.video.repository.api.TmdbVideoApi
+import com.stardeux.upprime.tmdb.video.repository.model.VideoMapper
+import com.stardeux.upprime.tmdb.video.usecase.VideoUseCase
 import org.koin.core.qualifier.named
 import org.koin.dsl.module
 import retrofit2.Retrofit
+import java.util.*
 
 val videoModule = module {
     factory { provideVideoApi(get(named(TMDB_NAMED_QUALIFIER))) }
-    factory { provideVideoRepository(get()) }
+    factory { provideVideoRepository(get(), get()) }
+    factory { provideVideoMapper() }
+    factory { provideVideoUseCase(get(), get()) }
 }
 
 private fun provideVideoApi(retrofit: Retrofit): TmdbVideoApi {
     return retrofit.create(TmdbVideoApi::class.java)
 }
 
-private fun provideVideoRepository(tmdbVideoApi: TmdbVideoApi): VideoRepository {
-    return VideoRepository(tmdbVideoApi)
+private fun provideVideoMapper(): VideoMapper {
+    return VideoMapper()
+}
+
+private fun provideVideoRepository(tmdbVideoApi: TmdbVideoApi, videoMapper: VideoMapper): VideoRepository {
+    return VideoRepository(tmdbVideoApi, videoMapper)
+}
+
+private fun provideVideoUseCase(videoRepository: VideoRepository, locale: Locale): VideoUseCase {
+    return VideoUseCase(videoRepository, locale)
 }
--- app/src/main/java/com/stardeux/upprime/tmdb/video/usecase/VideoUseCase.kt
+++ app/src/main/java/com/stardeux/upprime/tmdb/video/usecase/VideoUseCase.kt
@@ -1,10 +1,14 @@
 package com.stardeux.upprime.tmdb.video.usecase
 
+import com.stardeux.upprime.core.model.MediaType
+import com.stardeux.upprime.core.model.TmdbId
 import com.stardeux.upprime.tmdb.video.repository.VideoRepository
+import com.stardeux.upprime.tmdb.video.repository.model.Video
+import java.util.*
 
-class VideoUseCase (videoRepository: VideoRepository) {
+class VideoUseCase(private val videoRepository: VideoRepository, private val locale: Locale) {
 
-    fun getVideos() {
-
+    suspend fun getVideos(mediaType: MediaType, tmdbId: TmdbId): List<Video>? {
+        return videoRepository.getVideos(mediaType, tmdbId, locale)
     }
 }
