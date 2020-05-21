e7fa27bb71e621749bfb1ae8fefc15934a41e0f8
Jeremy Thil <thil.jeremy@gmail.com>
2020-05-21 21:26:26 +0200

    inject series credit requirement

--- app/src/main/java/com/stardeux/upprime/tmdb/credit/di/Injection.kt
+++ app/src/main/java/com/stardeux/upprime/tmdb/credit/di/Injection.kt
@@ -4,16 +4,46 @@ import com.stardeux.upprime.network.tmdb.di.TMDB_NAMED_QUALIFIER
 import com.stardeux.upprime.tmdb.common.mapper.PosterMapper
 import com.stardeux.upprime.tmdb.credit.repository.MediaCreditRepository
 import com.stardeux.upprime.tmdb.credit.repository.MediaCreditMapper
+import com.stardeux.upprime.tmdb.credit.repository.SeriesCreatorRepository
 import com.stardeux.upprime.tmdb.credit.repository.api.TmdbCreditApi
 import com.stardeux.upprime.tmdb.credit.usecase.MovieCreditUseCase
+import com.stardeux.upprime.tmdb.credit.usecase.SeriesCreatorUseCase
+import com.stardeux.upprime.tmdb.credit.usecase.SeriesCreditsUseCase
+import com.stardeux.upprime.tmdb.series.repository.api.SeriesRemoteDataSource
 import org.koin.core.qualifier.named
 import org.koin.dsl.module
 import retrofit2.Retrofit
+import java.util.*
 
 val creditModule = module {
     factory { provideTmdbCreditApi(get(named(TMDB_NAMED_QUALIFIER))) }
     factory { provideMediaCreditMapper(get()) }
     factory { provideMediaCreditRepository(get(), get()) }
+    factory { provideMovieCreditUseCase(get()) }
+
+    factory { provideSeriesCreditUseCase(get(), get()) }
+    factory { provideSeriesCreatorUseCase(get(), get()) }
+    factory { provideSeriesCreatorRepository(get(), get()) }
+
+}
+
+private fun provideSeriesCreatorRepository(
+    seriesRemoteDataSource: SeriesRemoteDataSource,
+    mediaCreditMapper: MediaCreditMapper
+): SeriesCreatorRepository {
+    return SeriesCreatorRepository(seriesRemoteDataSource, mediaCreditMapper)
+}
+
+private fun provideSeriesCreatorUseCase(
+    seriesCreatorRepository: SeriesCreatorRepository, locale: Locale
+): SeriesCreatorUseCase {
+    return SeriesCreatorUseCase(seriesCreatorRepository, locale)
+}
+
+private fun provideSeriesCreditUseCase(
+    mediaCreditRepository: MediaCreditRepository, seriesCreatorUseCase: SeriesCreatorUseCase
+): SeriesCreditsUseCase {
+    return SeriesCreditsUseCase(mediaCreditRepository, seriesCreatorUseCase)
 }
 
 
@@ -26,12 +56,11 @@ private fun provideMediaCreditMapper(posterMapper: PosterMapper): MediaCreditMap
 }
 
 private fun provideMediaCreditRepository(
-    creditApi: TmdbCreditApi,
-    mediaCreditMapper: MediaCreditMapper
+    creditApi: TmdbCreditApi, mediaCreditMapper: MediaCreditMapper
 ): MediaCreditRepository {
     return MediaCreditRepository(creditApi, mediaCreditMapper)
 }
 
-private fun provideMediaCreditUseCase(mediaCreditRepository: MediaCreditRepository): MovieCreditUseCase {
+private fun provideMovieCreditUseCase(mediaCreditRepository: MediaCreditRepository): MovieCreditUseCase {
     return MovieCreditUseCase(mediaCreditRepository)
 }
