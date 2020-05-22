07a496e18cf9d4b117a64ba8bfd1d07c9cc00cb2
Jeremy Thil <thil.jeremy@gmail.com>
2020-05-22 15:44:51 +0200

    coroutine child for video, casting

--- app/src/main/java/com/stardeux/upprime/media/fiche/ui/MediaFicheViewModel.kt
+++ app/src/main/java/com/stardeux/upprime/media/fiche/ui/MediaFicheViewModel.kt
@@ -1,5 +1,6 @@
 package com.stardeux.upprime.media.fiche.ui
 
+import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
@@ -8,13 +9,15 @@ import com.stardeux.upprime.core.ui.SingleLiveEvent
 import com.stardeux.upprime.media.common.repository.model.ShortMedia
 import com.stardeux.upprime.media.common.ui.GetImdbMediaDetailsUseCaseFacade
 import com.stardeux.upprime.media.fiche.ui.model.MediaFicheUi
-import com.stardeux.upprime.tmdb.video.ui.model.MediaVideoMapper
-import com.stardeux.upprime.tmdb.video.ui.model.MediaVideoUi
 import com.stardeux.upprime.tmdb.common.request.mapToImdbMediaRequest
 import com.stardeux.upprime.tmdb.credit.ui.CreditUseCaseFacade
 import com.stardeux.upprime.tmdb.credit.ui.model.CreditUi
+import com.stardeux.upprime.tmdb.video.ui.model.MediaVideoMapper
+import com.stardeux.upprime.tmdb.video.ui.model.MediaVideoUi
 import com.stardeux.upprime.tmdb.video.usecase.VideoUseCase
+import kotlinx.coroutines.CoroutineExceptionHandler
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.supervisorScope
 
 class MediaFicheViewModel(
     private val getImdbMediaDetailsUseCaseFacade: GetImdbMediaDetailsUseCaseFacade,
@@ -36,20 +39,30 @@ class MediaFicheViewModel(
     val videoClicked : LiveData<MediaVideoUi> = _videoClicked
 
     fun load(shortMedia: ShortMedia) {
-        viewModelScope.launch {
-            val imdbMediaRequest = mapToImdbMediaRequest(shortMedia)
+        val exceptionHandler = CoroutineExceptionHandler { _, throwable ->
+            Log.e("FicheVM", "error", throwable)
+        }
 
-            val mediaDetails =
-                getImdbMediaDetailsUseCaseFacade.getDetails(shortMedia.type, imdbMediaRequest)
-            _mediaItemUi.value = mediaDetails
+        viewModelScope.launch(exceptionHandler) {
+            supervisorScope {
+                val imdbMediaRequest = mapToImdbMediaRequest(shortMedia)
 
-            val mediaVideos = videoUseCase.getVideos(shortMedia.type, mediaDetails.tmdbId)
-            _videos.value = mediaVideos?.map {
-                mediaVideoMapper.mapToMediaVideoUi(it, ::onMediaVideoUiClicked)
+                val mediaDetails =
+                    getImdbMediaDetailsUseCaseFacade.getDetails(shortMedia.type, imdbMediaRequest)
+                _mediaItemUi.value = mediaDetails
+
+                launch {
+                    val mediaVideos = videoUseCase.getVideos(shortMedia.type, mediaDetails.tmdbId)
+                    _videos.value = mediaVideos?.map {
+                        mediaVideoMapper.mapToMediaVideoUi(it, ::onMediaVideoUiClicked)
+                    }
+                }
+
+                launch {
+                    val mediaCredits = creditUseCaseFacade.getCredits(shortMedia.type, mediaDetails.tmdbId)
+                    _credits.value = mediaCredits
+                }
             }
-
-            val mediaCredits = creditUseCaseFacade.getCredits(shortMedia.type, mediaDetails.tmdbId)
-            _credits.value = mediaCredits
         }
     }
 
