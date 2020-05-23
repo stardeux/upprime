5a994c09af791c54833e7857334d113c4f72976e
Jeremy Thil <thil.jeremy@gmail.com>
2020-05-23 14:18:09 +0200

    start search ui

--- app/src/main/java/com/stardeux/upprime/search/ui/SearchActivity.kt
+++ app/src/main/java/com/stardeux/upprime/search/ui/SearchActivity.kt
@@ -2,9 +2,24 @@ package com.stardeux.upprime.search.ui
 
 import android.content.Context
 import android.content.Intent
+import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.commit
+import com.stardeux.upprime.R
+import com.stardeux.upprime.core.extension.applyCommonBack
+import kotlinx.android.synthetic.main.activity_media_fiche.*
 
-class SearchActivity : AppCompatActivity() {
+class SearchActivity : AppCompatActivity(R.layout.activity_search) {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        applyCommonBack(toolbar)
+
+        supportFragmentManager.commit {
+            replace(R.id.searchContent, SearchFragment.newInstance())
+        }
+    }
 
     companion object {
         fun newIntent(context: Context): Intent {
--- app/src/main/java/com/stardeux/upprime/search/ui/SearchFragment.kt
+++ app/src/main/java/com/stardeux/upprime/search/ui/SearchFragment.kt
@@ -1,9 +1,16 @@
 package com.stardeux.upprime.search.ui
 
+import android.os.Bundle
 import androidx.fragment.app.Fragment
 import com.stardeux.upprime.R
+import com.stardeux.upprime.core.extension.applyCommonBack
+import kotlinx.android.synthetic.main.activity_media_fiche.*
 
 class SearchFragment : Fragment(R.layout.fragment_search) {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
 
     companion object {
         fun newInstance(): SearchFragment {
