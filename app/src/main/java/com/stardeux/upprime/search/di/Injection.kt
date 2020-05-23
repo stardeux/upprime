995e6309e9182fc04be3c62aa4cc2c19fa0b56b9
Jeremy Thil <thil.jeremy@gmail.com>
2020-05-23 13:07:10 +0200

    search usecase

--- app/src/main/java/com/stardeux/upprime/search/di/Injection.kt
+++ app/src/main/java/com/stardeux/upprime/search/di/Injection.kt
@@ -1,9 +1,11 @@
 package com.stardeux.upprime.search.di
 
+import com.stardeux.upprime.country.usecase.model.AvailableCountry
 import com.stardeux.upprime.network.amazon.di.AMAZON_NAMED_QUALIFIER
 import com.stardeux.upprime.search.repository.AmazonSearchRepository
 import com.stardeux.upprime.search.repository.api.AmazonSearchApi
 import com.stardeux.upprime.search.repository.model.AmazonSearchMediaMapper
+import com.stardeux.upprime.search.usecase.AmazonSearchUseCase
 import org.koin.core.qualifier.named
 import org.koin.dsl.module
 import retrofit2.Retrofit
@@ -12,6 +14,11 @@ val searchModule = module {
     factory { provideAmazonSearchApi(get(named(AMAZON_NAMED_QUALIFIER))) }
     factory { provideAmazonSearchMediaMapper() }
     factory { provideAmazonSearchRepository(get(), get()) }
+
+    scope<AvailableCountry> {
+        factory { provideAmazonSearchUseCase(get(), get()) }
+    }
+
 }
 
 private fun provideAmazonSearchApi(retrofit: Retrofit): AmazonSearchApi {
@@ -26,4 +33,8 @@ private fun provideAmazonSearchRepository(
     amazonSearchApi: AmazonSearchApi, amazonSearchMediaMapper: AmazonSearchMediaMapper
 ): AmazonSearchRepository {
     return AmazonSearchRepository(amazonSearchApi, amazonSearchMediaMapper)
+}
+
+private fun provideAmazonSearchUseCase(amazonSearchRepository: AmazonSearchRepository, availableCountry: AvailableCountry) : AmazonSearchUseCase {
+    return AmazonSearchUseCase(amazonSearchRepository, availableCountry)
 }
