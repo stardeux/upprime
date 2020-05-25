a6fb8865f294a4a6e6a4b3ab247ec4240206cc97
Jeremy Thil <thil.jeremy@gmail.com>
2020-05-25 15:22:09 +0200

    picker ui

--- app/src/main/java/com/stardeux/upprime/search/ui/SearchFragment.kt
+++ app/src/main/java/com/stardeux/upprime/search/ui/SearchFragment.kt
@@ -2,31 +2,23 @@ package com.stardeux.upprime.search.ui
 
 import android.graphics.Color
 import android.os.Bundle
-import android.provider.MediaStore
 import android.view.View
-import android.widget.NumberPicker
-import android.widget.SeekBar
 import androidx.annotation.IdRes
 import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.shawnlin.numberpicker.NumberPicker
 import com.stardeux.upprime.R
-import com.stardeux.upprime.core.extension.colorDivider
 import com.stardeux.upprime.core.extension.observeNotNull
-import com.stardeux.upprime.core.extension.onValueChanged
-import com.stardeux.upprime.core.ui.SpacesItemDecoration
 import com.stardeux.upprime.media.common.repository.model.ShortMedia
 import com.stardeux.upprime.media.fiche.ui.MediaFicheActivity
 import com.stardeux.upprime.search.ui.list.SearchResultAdapter
 import com.stardeux.upprime.search.ui.model.AmazonSearchResultUi
 import com.stardeux.upprime.search.ui.model.MediaTypeFilter
 import com.stardeux.upprime.search.ui.model.YearIntervalUi
-import com.stardeux.upprime.search.usecase.model.AmazonSearchResultContainer
 import kotlinx.android.synthetic.main.fragment_search.*
-import org.koin.android.ext.android.inject
 import org.koin.androidx.viewmodel.ext.android.getViewModel
-import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class SearchFragment : Fragment(R.layout.fragment_search) {
 
@@ -40,17 +32,12 @@ class SearchFragment : Fragment(R.layout.fragment_search) {
             adapter = SearchResultAdapter()
         }
 
-        startYearPicker.colorDivider(Color.TRANSPARENT)
-        startYearPicker.wrapSelectorWheel = false
-        startYearPicker.setOnValueChangedListener { _, _, newVal ->
-            searchViewModel.onYearStartChanged(newVal)
+        initNumberPicker(startYearPicker) { _, _, newVal ->
+            searchViewModel.onYearStartChanged(
+                newVal
+            )
         }
-
-        endYearPicker.colorDivider(Color.TRANSPARENT)
-        endYearPicker.wrapSelectorWheel = false
-        endYearPicker.setOnValueChangedListener { _, _, newVal ->
-            searchViewModel.onYearEndChanged(newVal)
-        }
+        initNumberPicker(endYearPicker) { _, _, newVal -> searchViewModel.onYearEndChanged(newVal) }
 
         mediaTypeFiltersGroup.setOnCheckedChangeListener { _, checkedId ->
             searchViewModel.onMediaTypeFilterChanged(mapRadioButtonIdToFilter(checkedId))
@@ -63,6 +50,16 @@ class SearchFragment : Fragment(R.layout.fragment_search) {
             startYearInterval.observeNotNull(viewLifecycleOwner, ::handleStartYearInterval)
             endYearInterval.observeNotNull(viewLifecycleOwner, ::handleEndYearInterval)
             searchResultClicked.observeNotNull(viewLifecycleOwner, ::handleSearchResultClicked)
+        }
+    }
+
+    private fun initNumberPicker(
+        numberPicker: NumberPicker,
+        onValueChangeListener: (NumberPicker, Int, Int) -> Unit
+    ) {
+        with(numberPicker) {
+            formatter = NumberPicker.Formatter { number -> number.toString() }
+            setOnValueChangedListener(onValueChangeListener)
         }
     }
 
