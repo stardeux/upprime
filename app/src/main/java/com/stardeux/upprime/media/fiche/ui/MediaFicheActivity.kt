d66fd091793b90d560b5cceffe54f7d316fdd2b4
Jeremy Thil <thil.jeremy@gmail.com>
2020-05-31 15:24:58 +0200

    set activity toolbar

--- app/src/main/java/com/stardeux/upprime/media/fiche/ui/MediaFicheActivity.kt
+++ app/src/main/java/com/stardeux/upprime/media/fiche/ui/MediaFicheActivity.kt
@@ -15,6 +15,7 @@ class MediaFicheActivity : AppCompatActivity(R.layout.activity_media_fiche) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        setSupportActionBar(toolbar)
         applyCommonBack(toolbar)
 
         val shortMedia: ShortMedia = requireNotNull(intent.getParcelableExtra(MEDIA_ARG))
