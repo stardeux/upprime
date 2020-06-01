98862faa1d55b7cd657fadfedef26f9b0c3c41dd
Jeremy Thil <thil.jeremy@gmail.com>
2020-06-01 16:45:18 +0200

    add banner programmatically

--- app/src/main/java/com/stardeux/upprime/home/ui/HomeActivity.kt
+++ app/src/main/java/com/stardeux/upprime/home/ui/HomeActivity.kt
@@ -3,10 +3,14 @@ package com.stardeux.upprime.home.ui
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import android.view.Gravity
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import androidx.coordinatorlayout.widget.CoordinatorLayout
 import androidx.fragment.app.commit
 import com.google.android.gms.ads.AdRequest
+import com.google.android.gms.ads.AdSize
+import com.google.android.gms.ads.AdView
 import com.stardeux.upprime.R
 import kotlinx.android.synthetic.main.activity_home.*
 
@@ -28,7 +32,28 @@ class HomeActivity : AppCompatActivity(R.layout.activity_home) {
             }
         }
 
-        adView.loadAd(AdRequest.Builder().build())
+        loadBottomBanner()
+    }
+
+    private fun loadBottomBanner() {
+        val adView = AdView(this).apply {
+            adUnitId = "ca-app-pub-3940256099942544/6300978111"
+            adSize = AdSize.BANNER
+
+            val coordinatorLayoutParams = CoordinatorLayout.LayoutParams(
+                CoordinatorLayout.LayoutParams.MATCH_PARENT,
+                CoordinatorLayout.LayoutParams.WRAP_CONTENT
+            ).apply {
+                anchorId = R.id.mainContent
+                anchorGravity = Gravity.BOTTOM
+            }
+
+            layoutParams = coordinatorLayoutParams
+
+            loadAd(AdRequest.Builder().build())
+        }
+
+        homeCoordinator.addView(adView)
     }
 
     companion object {
